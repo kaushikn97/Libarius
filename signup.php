59681584fc57f0e2eee87cedf314f9ed812d3122
kaushikn97 <kaushikn97@gmail.com>
2018-04-24 22:05:28 +0530

    adf

--- signup.php
+++ signup.php
@@ -8,7 +8,7 @@
     $name = $_POST["sign_name"];
     $id = $_POST["sign_id"];
     $pass = $_POST["sign_pass"];
-    
+
     include 'db_conn_test.php';
 
     $conn = OpenCon();
@@ -17,9 +17,9 @@
     //echo $name;
     //echo $id;
     //echo $pass;
-    
+
     $error = 0;
-    
+
     if(!preg_match("/^201+[0-9]{1}+[AB]{1}+[0-9]{1}+[AP]{1}+[S0-9]{1}+0[0-9]{3}+H$/",$id)){
         $error = 1;
         echo "Error";
